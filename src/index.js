da1a09dca7bbca38f24e137ddb28af3db568c0e5
donutdiet <72674098+donutdiet@users.noreply.github.com>
2024-11-25 00:09:21 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import "./styles.css";
+
+console.log("W project");
---
+++ webpack.common.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: "./src/index.js",
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        // for local images linked in HTML template
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        // for local images imported in JavaScript
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
